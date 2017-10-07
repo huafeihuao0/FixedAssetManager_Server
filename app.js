11f1201870128638a13fa46769a1ece4c99f21b1
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-07 12:38:39 +0800

    重构app.js应用入口文件

--- app.js
+++ app.js
@@ -23,93 +23,179 @@
   Desc: app - the server
  */
 
-var fs           = require("fs");
-var path         = require("path");
-var express      = require("express");
-var routes       = require("./routes");
-var common       = require("./common/Error");
-var AppConfig    = require("./appConfig").config;
-var Loader       = require("loader");
-var errorHandler = require("./common/errorHandler");
+var fs = require("fs");//fs文件服务
+var pathParser = require("path");//路径解析
+var express = require("express");
+var routes = require("./routes");//路由
+var common = require("./common/Error");//通用模块
+var AppConfig = require("./appConfig").config;//应用配置脚本
+var Loader = require("loader");//loader静态
+var errorHandler = require("./common/errorHandler");//错误处理器
+var cronService = require("./services/cron");
 
-var cronService  = require("./services/cron");
 
+var app;
+var maxAge = 3600000 * 24 * 30;
+var staticDir = pathParser.join(__dirname, 'public');
+
+(function init()
+{
+    //创建服务器实例
+    app = express.createServer();
+    /*加载静态资源*/
+    loadStaticAssets();
+    /*配置app环境*/
+    configApp();
+    /*使用自定义指定路由中间件*/
+    useCustomeMidwares();
+    /*设置app助手*/
+    setupAppHelpers();
+    /*配置开发环境*/
+    configDevEnv();
+    /*配置生成环境*/
+    configProEnv();
+    //设置全局的错误处理器
+    errorHandler.appErrorProcess(app);
+    /*配置路由*/
+    routes(app);
+    //启动监听
+    startListening();
+    /*启动守护进程*/
+    startDemon();
+})();
+
+function ____静态资源____() {}//
 var assets = {};
-if (AppConfig.mini_assets) {
-    try {
-        assets = JSON.parse(fs.readFileSync(path.join(__dirname, 'assets.json')));
-    } catch (e) {
-        console.log('You must execute `make build` before start app when mini_assets is true.');
-        throw e;
+
+/***
+ *加载静态资源
+ ***/
+function loadStaticAssets()
+{
+    if (AppConfig.mini_assets)
+    {
+        try
+        {
+            assets = JSON.parse(fs.readFileSync(pathParser.join(__dirname, 'assets.json')));
+        } catch (e)
+        {
+            console.log('You must execute `make build` before start app when mini_assets is true.');
+            throw e;
+        }
     }
 }
 
-var app          = express.createServer();
 
-//config for all env
-app.configure(function () {
+/***
+ *配置app环境
+ ***/
+function configApp()
+{
+    app.configure(function ()
+    {
+        /*注册模板引擎*/
+        registerTempEngine();
+        /*使用前置中间件*/
+        usePreMidwares();
+    });
+}
+
+/***
+ *注册模板引擎
+ ***/
+function registerTempEngine()
+{
     app.set('view engine', 'html');
-    app.set('views', path.join(__dirname, 'views'));
-    app.set("view options", {layout : false});
+    app.set('views', pathParser.join(__dirname, 'views'));
+    app.set("view options", {layout: false});
     app.register('.html', require('ejs'));
+}
 
-    //middleware
+/***
+ *使用前置中间件
+ ***/
+function usePreMidwares()
+{
     app.use(express.compress());
     app.use(express.favicon());
-    
     app.use(express.query());
-    app.use(express.bodyParser({ uploadDir : "./uploads"}));
+    app.use(express.bodyParser({uploadDir: "./uploads"}));
 
     app.use(express.cookieParser());
     app.use(express.session({
-        secret : AppConfig.session_secret,
-        cookie : {
-            maxAge  : 30 * 60 * 1000      //ms
+        secret: AppConfig.session_secret,
+        cookie: {
+            maxAge: 30 * 60 * 1000      //ms
         }
     }));
-});
+}
 
-//custom middleware
-app.use(require("./controllers/login").commonProcess);
+/***
+ *使用自定义指定路由中间件
+ ***/
+function useCustomeMidwares()
+{
+    app.use(require("./controllers/login").commonProcess);
+}
 
-var maxAge = 3600000 * 24 * 30;
-var staticDir = path.join(__dirname, 'public');
+/***
+ *设置app助手
+ ***/
+function setupAppHelpers()
+{
+    app.helpers({
+        config: AppConfig,
+        Loader: Loader,
+        assets: assets
+    });
+}
 
-//set static, dynamic helpers
-app.helpers({
-    config: AppConfig,
-    Loader: Loader,
-    assets: assets
-});
+/***
+ *配置开发环境
+ ***/
+function configDevEnv()
+{
+    app.configure('development', function ()
+    {
+        app.use(express.logger());
+        app.use("/public", express.static(staticDir));
+        app.use(express.errorHandler(
+            {showStack: true, dumpException: true}
+        ));
+    });
+}
 
-//config for devp env
-app.configure('development', function () {
-    app.use(express.logger());
-    app.use("/public", express.static(staticDir));
-    app.use(express.errorHandler(
-        { showStack : true, dumpException : true }
-    ));
-});
+/***
+ *配置生成环境
+ ***/
+function configProEnv()
+{
+    app.configure("production", function ()
+    {
+        app.use('/public', express.static(staticDir, {maxAge: maxAge}));
+        app.use(express.errorHandler());
+        app.set('view cache', true);
+    });
+}
 
-//config for production env
-app.configure("production", function () {
-    app.use('/public', express.static(staticDir, { maxAge: maxAge }));
-    app.use(express.errorHandler());
-    app.set('view cache', true);
-});
-
-//error hanlder
-errorHandler.appErrorProcess(app);
-
-routes(app);
-
-//launch it!
-app.listen(AppConfig.port);
-console.log("the app server run at port :%d in %s mode. ", AppConfig.port, app.settings.env);
-
-module.exports = app;
+/***
+ *启动监听
+ ***/
+function startListening()
+{
+    app.listen(AppConfig.port);
+    console.log("the app server run at port :%d in %s mode. ", AppConfig.port, app.settings.env);
+}
 
 //start deamon services
-cronService.startLimatationMailNotification(AppConfig.limitCronPattern);
-cronService.startDBBackupService(AppConfig.backupCronPattern);
-cronService.startPushDBBackupFileService(AppConfig.backupPushCronPattern);
+/***
+ *启动守护进程
+ ***/
+function startDemon()
+{
+    cronService.startLimatationMailNotification(AppConfig.limitCronPattern);
+    cronService.startDBBackupService(AppConfig.backupCronPattern);
+    cronService.startPushDBBackupFileService(AppConfig.backupPushCronPattern);
+}
+
+module.exports = app;
